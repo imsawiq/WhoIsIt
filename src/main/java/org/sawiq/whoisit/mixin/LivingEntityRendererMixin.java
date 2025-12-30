da4d942d2dbfedf7b83e72e30fae5cb2800dffe2
imsawiq <promalinagggg@gmail.com>
2025-12-31 00:24:03 +0300

    1.21.9

--- src/main/java/org/sawiq/whoisit/mixin/LivingEntityRendererMixin.java
+++ src/main/java/org/sawiq/whoisit/mixin/LivingEntityRendererMixin.java
@@ -1,8 +1,7 @@
-package org.sawiq.whoisit.mixin;
-
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import org.sawiq.whoisit.config.WhoisitConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
+
     @Inject(
             method = "hasLabel(Lnet/minecraft/entity/LivingEntity;D)Z",
             at = @At("HEAD"),
@@ -18,12 +18,12 @@ public class LivingEntityRendererMixin {
     )
     private void whoisit$modifyNametagVisibility(LivingEntity entity, double distance, CallbackInfoReturnable<Boolean> cir) {
         MinecraftClient client = MinecraftClient.getInstance();
+        Entity camera = client.getCameraEntity(); // вместо client.cameraEntity
+        if (camera == null) return;
 
-        if (WhoisitConfig.enabledOwnName && entity == client.cameraEntity) {
+        if (WhoisitConfig.enabledOwnName && entity == camera) {
             cir.setReturnValue(MinecraftClient.isHudEnabled());
-        }
-
-        if (WhoisitConfig.enabledOtherPlayersName && entity != client.cameraEntity) {
+        } else if (WhoisitConfig.enabledOtherPlayersName && entity != camera) {
             cir.setReturnValue(MinecraftClient.isHudEnabled());
         }
     }
